8ff01fca60bdcf039d5c42a7cfc5e901b1890b36
Vlad Vorobei <vorobei.dev@gmail.com>
2025-02-05 19:23:11 +0200

    Added .env

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,17 +1,21 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config:
     DATABASE_URL = os.getenv(
         "DATABASE_URL", "postgresql+asyncpg://admin:admin@localhost:5432/hw8"
     )
-    JWT_SECRET = os.getenv("SECRET_KEY", "YOUR_SECRET_KEY")
-    JWT_ALGORITHM = "HS256"
-    JWT_EXPIRATION_SECONDS = 3600
+    JWT_SECRET = os.getenv("JWT_SECRET", "YOUR_SECRET_KEY")
+    JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+    JWT_EXPIRATION_SECONDS = os.getenv("JWT_EXPIRATION_SECONDS", 3600)
 
-    CLD_NAME: str = "duumzmt1u"
-    CLD_API_KEY: int = 156282568713174
-    CLD_API_SECRET: str = "GKkk6Ru45Kg6qFr7RkY6WHlAaOI"
+    CLD_NAME: str = os.getenv("CLD_NAME", "asdasdasd")
+    CLD_API_KEY: int = os.getenv("CLD_API_KEY", 156282568713174)
+    CLD_API_SECRET: str = os.getenv("CLD_API_SECRET", "GREGFDGSDSFWEF")
 
 
 config = Config
